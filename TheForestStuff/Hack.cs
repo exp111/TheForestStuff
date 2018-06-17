0d829c0072bd954575e0a5b9d8fe8fe825aeb630
Exp <theexp111@gmail.com>
2018-06-17 20:08:38 +0200

    fuckme

--- TheForestStuff/Hack.cs
+++ TheForestStuff/Hack.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
@@ -14,7 +15,7 @@ namespace TheForestStuff
 		public bool _menuVisible = true;
 		public Rect _menuRect = new Rect(10, 10, 300, 300);
 		//Items
-		public bool _itemMenuVisible = true;
+		public bool _itemMenuVisible = false;
 		public Rect _itemMenuRect = new Rect(350, 10, 250, 500);
 		public Vector2 _itemScrollPosition = Vector2.zero;
 
@@ -79,7 +80,7 @@ namespace TheForestStuff
 			GUI.DragWindow(new Rect(0, 0, _itemMenuRect.width, 20));
 
 			TheForest.Items.Item[] items = TheForest.Items.ItemDatabase.Items;
-			_itemScrollPosition = GUI.BeginScrollView(new Rect(10, 10, _itemMenuRect.width, _itemMenuRect.height), _itemScrollPosition, new Rect(0, 0, _itemMenuRect.width - 20, items.Length * 20));
+			_itemScrollPosition = GUI.BeginScrollView(new Rect(10, 10, _itemMenuRect.width, _itemMenuRect.height), _itemScrollPosition, new Rect(0, 0, _itemMenuRect.width - 10, items.Length * 20));
 			var y = 0;
 			foreach (TheForest.Items.Item item in items)
 			{
@@ -90,6 +91,15 @@ namespace TheForestStuff
 				}
 			}
 			GUI.EndScrollView();
+		}
+
+		Dictionary<string,TheForest.Player.AchievementData> GetAchievements(object obj)
+		{
+				return obj.GetType().
+						GetFields(BindingFlags.Public | BindingFlags.Static).
+						Where(f => f.FieldType == typeof(TheForest.Player.AchievementData)).
+						ToDictionary(f => f.Name,
+									f => (TheForest.Player.AchievementData)f.GetValue(null));
 		}
 	}
 }
